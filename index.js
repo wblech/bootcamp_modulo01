a8520f8b70bc146a490ca663c5baaf92e12321e5
Wincenty Bertoni Lech <8575320+wblech@users.noreply.github.com>
2019-06-24 20:16:39 -0300

    Add Create new task

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ server.use(express.json());
 
 const projects = [{ id: '1', title: 'Primeiro Título', tasks: ['fazer lição', 'jogar bola'] }];
 
+// Get all posts
 server.get('/projects', (req, res) => res.json(projects));
 
+// Create new post
 server.post('/projects', (req, res) => {
   const { id, title } = req.body;
   const project = { id, title, tasks: [] };
@@ -22,6 +24,15 @@ server.put('/projects/:id', (req, res) => {
 
   const project = projects.find(p => p.id === id);
   project.title = title;
+  res.send(projects);
+});
+
+// Add new task
+server.put('/projects/:id/task', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  const project = projects.find(p => p.id === id);
+  project.tasks.push(title);
   res.send(projects);
 });
 
