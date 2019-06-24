bb4423e0d72dd2b6e732f93ef707fc6e48670726
Wincenty Bertoni Lech <Wblech@MacBook-Pro-de-Wincenty.local>
2019-06-24 19:59:19 -0300

    Add PUT fot changing title name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = express();
 
 server.use(express.json());
 
-const projects = [];
+const projects = [{ id: '1', title: 'Primeiro Título', tasks: ['fazer lição', 'jogar bola'] }];
 
 server.get('/projects', (req, res) => res.json(projects));
 
@@ -14,4 +14,15 @@ server.post('/projects', (req, res) => {
   projects.push(project);
   return res.json(projects);
 });
+
+// Change title name
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id === id);
+  project.title = title;
+  res.send(projects);
+});
+
 server.listen(3000);
